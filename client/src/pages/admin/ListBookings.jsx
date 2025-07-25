976fb9e0d53dfb0aad4331655734397ea5a01b0e
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-07-25 22:09:45 +0530

    admin side making

---
+++ client/src/pages/admin/ListBookings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListBookings = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ListBookings
---
+++ client/src/pages/admin/ListShows.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListShows = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ListShows
